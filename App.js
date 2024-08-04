6312f52d498bd8e0bd9c844b0f50bad8a956fe30
BereketMengaw <165939233+BereketMengaw@users.noreply.github.com>
2024-08-04 16:55:38 +0300

    git ignore cancelled

--- App.js
+++ App.js
@@ -4,12 +4,26 @@ import "./commonResource/css/styles.css";
 
 import Header from "./functional component/Header";
 import Footer from "./functional component/Footer";
+import First from "./functional component/First";
+import Alert from "./functional component/Alert";
+import Second from "./functional component/Second";
+import Third from "./functional component/Third";
+import Forth from "./functional component/Forth";
+import Fifth from "./functional component/Fifth";
+import Sixth from "./functional component/Sixth";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <App />
+      <Alert />
+      <First />
+      <Second />
+      <Third />
+      <Forth />
+      <Fifth />
+      <Sixth />
+
       <Footer />
     </div>
   );
